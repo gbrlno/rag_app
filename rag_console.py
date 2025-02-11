44c6854ae37f28c870e6a6f1af4426a82f8d85b6
gbrlno <gbrlno@gmail.com>
2025-02-11 19:04:07 +0800

    add embeddings to chromadb

--- rag_console.py
+++ rag_console.py
@@ -3,6 +3,10 @@ import sys
 
 from pprint import pprint
 
+import chromadb
+
+from chromadb.utils.embedding_functions.ollama_embedding_function import OllamaEmbeddingFunction
+
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -22,6 +26,49 @@ def split_documents(documents):
         is_separator_regex=False,
     )
     return text_splitter.split_documents(documents)
+
+def get_vector_collection(collection_name_current):
+    ollama_ef = OllamaEmbeddingFunction(
+        url="http://localhost:11434/api/embeddings",
+        model_name="nomic-embed-text:latest",
+    )
+
+    chroma_client = chromadb.PersistentClient(path=path_chroma)
+    return chroma_client.get_or_create_collection(
+        name=collection_name_current,
+        embedding_function=ollama_ef,
+        metadata={"hnsw:space": "cosine"},
+    )
+
+def add_to_vector_collection(collection_name_current, chunks):
+    collection = get_vector_collection(collection_name_current)
+    documents, metadatas, ids = [], [], []
+
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+
+        documents.append(chunk.page_content)
+        metadatas.append(chunk.metadata)
+        ids.append(chunk_id)
+    
+    collection.upsert(
+        documents=documents,
+        metadatas=metadatas,
+        ids=ids,
+    )
 
 def main():
     #use python console_rag.py -p to make it process files in documents
@@ -47,7 +94,9 @@ def main():
         #process files in documents
         documents = load_documents(path_documents)
         chunks = split_documents(documents)
-        pprint(chunks, indent=4)
+        #pprint(chunks, indent=4)
+        add_to_vector_collection(collection_name_current, chunks)
+
         sys.exit()
 
 if __name__ == "__main__":
